bc47c78fe38a51bd522a18751a2aab1af8dbed0a
Sean Furlong <seanworkcomputer@Seans-MacBook-Pro.local>
2022-07-19 16:40:28 -0400

    walking skeleton

---
+++ src/main/java/server/EchoServer.java
@@ -0,0 +1,10 @@
+package server;
+
+public class EchoServer {
+    public static String walkingSkeletonTest(){
+        return "testing 123";
+    }
+    public static void main(String[] args) {
+        System.out.println(walkingSkeletonTest());
+    }
+}
---
+++ src/test/java/server/EchoServerTest.java
@@ -0,0 +1,12 @@
+package server;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class EchoServerTest {
+    @Test void walkingSkeletonTest() {
+        EchoServer classUnderTest = new EchoServer();
+        assertEquals(classUnderTest.walkingSkeletonTest(), "testing 123");
+    }
+}
